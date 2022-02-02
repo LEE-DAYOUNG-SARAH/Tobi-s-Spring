67d888c04472f9efa8bf1031895d29480c5e65da
Lee-Dayoung <ekdud8236@gmail.com>
2022-02-02 15:22:28 +0900

    tobis-spring 프로젝트 생성

---
+++ src/main/java/com/dayoung/demo/TobisSpringApplication.java
@@ -0,0 +1,13 @@
+package com.dayoung.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobisSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TobisSpringApplication.class, args);
+	}
+
+}
